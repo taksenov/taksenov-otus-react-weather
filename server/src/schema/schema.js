0fae04fa6801b2584a3069a2309bfbc86223373d
TAksenov <taksenov@gmail.com>
2021-08-01 17:02:54 +0500

    GQL-1: add CRUD operations in the GQL mutation

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -6,7 +6,6 @@ import {
   GraphQLInt,
   GraphQLList,
   GraphQLNonNull,
-  GraphQLBoolean,
 } from 'graphql';
 import mongoose from 'mongoose';
 
@@ -36,6 +35,7 @@ const FavouriteType = new GraphQLObjectType({
  */
 const Query = new GraphQLObjectType({
   name: 'Query',
+  description: 'GraphQL Query',
   fields: {
     favouriteCity: {
       description: 'Город из избранного',
@@ -45,6 +45,7 @@ const Query = new GraphQLObjectType({
         return FavouriteModel.findById(id);
       },
     },
+
     favouriteCities: {
       description: 'Список городов добавленных в избранное',
       type: new GraphQLList(FavouriteType),
@@ -55,7 +56,89 @@ const Query = new GraphQLObjectType({
   },
 });
 
+/**
+ * Мутация GraphQL Mutation
+ */
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'GraphQL Mutation',
+  fields: {
+    addFavouriteCity: {
+      description: 'Добавить город в избранное',
+      type: FavouriteType,
+      args: {
+        cityID: { type: new GraphQLNonNull(GraphQLInt) },
+        date: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        temperature: { type: new GraphQLNonNull(GraphQLString) },
+        feelsLike: { type: new GraphQLNonNull(GraphQLString) },
+        pressure: { type: new GraphQLNonNull(GraphQLString) },
+        humidity: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(
+        parent,
+        { cityID, date, name, temperature, feelsLike, pressure, humidity },
+      ) {
+        const favouriteCity = new FavouriteModel({
+          cityID,
+          date,
+          name,
+          temperature,
+          feelsLike,
+          pressure,
+          humidity,
+        });
+        return favouriteCity.save();
+      },
+    },
+
+    deleteFavouriteCity: {
+      description: 'Удалить город из избранного',
+      type: FavouriteType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, { id }) {
+        return FavouriteModel.findByIdAndRemove(id);
+      },
+    },
+
+    updateFavouriteCity: {
+      description: 'Обновить данные о городе в избранном',
+      type: FavouriteType,
+      args: {
+        id: { type: GraphQLID },
+        cityID: { type: new GraphQLNonNull(GraphQLInt) },
+        date: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        temperature: { type: new GraphQLNonNull(GraphQLString) },
+        feelsLike: { type: new GraphQLNonNull(GraphQLString) },
+        pressure: { type: new GraphQLNonNull(GraphQLString) },
+        humidity: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(
+        parent,
+        { id, cityID, date, name, temperature, feelsLike, pressure, humidity },
+      ) {
+        return FavouriteModel.findByIdAndUpdate(
+          id,
+          {
+            $set: {
+              cityID,
+              date,
+              name,
+              temperature,
+              feelsLike,
+              pressure,
+              humidity,
+            },
+          },
+          { new: true },
+        );
+      },
+    },
+  },
+});
+
 export const gqlSchema = new GraphQLSchema({
   query: Query,
-  // mutation: Mutation,
+  mutation: Mutation,
 });
